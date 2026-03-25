096e906d014582166dd8c81a617c349f35d1ef8e
MANNIELLO MICHELE <mikymanny015@gmail.com>
2026-03-25 11:42:18 +0100

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Michele Manniello on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
